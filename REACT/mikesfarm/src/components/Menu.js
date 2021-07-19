ccecf8aac2027ebd3b6d990bef2767560dac2334
kirk <kirk.betzina@gmail.com>
2021-07-19 11:39:59 -0400

    getting front end going for the restaurants menu

---
+++ REACT/mikesfarm/src/components/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Menu = (props) => {
+    return(
+        <div>
+            <h1>This is the Menu Component. I will dynamically render menu items from the database </h1>
+        </div>
+    )
+}
