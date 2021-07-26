85c62c6a2594a14cf751e25610c73a0201e582e6
kirk <kirk.betzina@gmail.com>
2021-07-26 13:34:26 -0400

    setting up the basic format of the website.

---
+++ REACT/mikesfarm/src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const AboutPage = () => {
+     return(
+         <div className= 'aboutme'>
+            <h1>About Mikes Munchies and Crunchies</h1>
+         </div>
+     )
+ }
+ export default AboutPage
---
+++ REACT/mikesfarm/src/pages/MainPage.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const MainPage = (props) => {
+    return(
+        <div ClassName='menudiv'>
+            <h1>this is the main page. this is where the menu will be displayed, </h1>
+        </div>
+    )
+}
+export default MainPage
