7fa9bb28ec0d9178b3e64d6d85054f3fbd129cac
kirk <kirk.betzina@gmail.com>
2021-07-26 17:32:31 -0400

    adding reactstrap things to the nav

--- REACT/mikesfarm/src/components/Nav.js
+++ REACT/mikesfarm/src/components/Nav.js
@@ -1,8 +1,58 @@
 import React from "react"
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
-const Nav = () => {
+const Nav = (props) => {
+
+    Nav.propTypes = {
+        tabs: PropTypes.bool,
+        pills: PropTypes.bool,
+        card: PropTypes.bool,
+        justified: PropTypes.bool,
+        fill: PropTypes.bool,
+        vertical: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+        horizontal: PropTypes.string,
+        navbar: PropTypes.bool,
+        tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+        // pass in custom element to use
+      }
+
+    NavItem.propTypes = {
+        tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+        active: PropTypes.bool,
+        // pass in custom element to use
+      }
+
+    NavLink.propTypes = {
+        disabled: PropTypes.bool,
+        active: PropTypes.bool,
+        // pass in custom element to use
+        tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+        // ref will only get you a reference to the NavLink component, use innerRef to get a reference to the DOM element (for things like focus management).
+        innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+      }
     return(
-        <div ClassName='nav'></div>
-    )
+        <div>
+      <p>List Based</p>
+      <Nav>
+        <NavItem>
+          <NavLink href="#">Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Another Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink disabled href="#">Disabled Link</NavLink>
+        </NavItem>
+      </Nav>
+      <hr />
+      <p>Link Based</p>
+      <Nav>
+        <NavLink href="#">Link</NavLink> <NavLink href="#">Link</NavLink> <NavLink href="#">Another Link</NavLink> <NavLink disabled href="#">Disabled Link</NavLink>
+      </Nav>
+    </div>
+    );
 }
 export default Nav
