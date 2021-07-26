a33bfa48074cabf0cc9977905436c3a6eec22158
kirk <kirk.betzina@gmail.com>
2021-07-26 14:51:32 -0400

    setting up the basic format of the website.

--- REACT/mikesfarm/src/pages/AboutPage.js
+++ REACT/mikesfarm/src/pages/AboutPage.js
@@ -4,6 +4,7 @@ import React from 'react'
      return(
          <div className= 'aboutme'>
             <h1>About Mikes Munchies and Crunchies</h1>
+            <h3>I would like to see a call to action here, add a comment or follow on socials... something</h3>
          </div>
      )
  }
